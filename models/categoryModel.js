7da5e69eef37c33598b1af1e0b2404bd5c304564
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-03-22 20:29:53 +0100

    Add function that gets id based rows

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -1,3 +1,6 @@
 const db = require('../db');
 
 exports.getAll = () => db.query('SELECT * FROM categories ORDER BY name');
+
+exports.getById = (id) =>
+  db.query('SELECT * FROM categories WHERE id = $1', [id]);
