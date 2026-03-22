478298041e813effd2d4e5631b11a301f75400eb
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-03-22 20:30:27 +0100

    Add function that adds an entry into the database

--- models/categoryModel.js
+++ models/categoryModel.js
@@ -4,3 +4,9 @@ exports.getAll = () => db.query('SELECT * FROM categories ORDER BY name');
 
 exports.getById = (id) =>
   db.query('SELECT * FROM categories WHERE id = $1', [id]);
+
+exports.create = (name, description) =>
+  db.query('INSERT INTO categories (name, description) VALUES ($1, $2)', [
+    name,
+    description,
+  ]);
