0d65e0b152ce0a3cb393314cc7cdcc15202936a7
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-03-25 21:03:25 +0100

    Add db query that add entriesin the table

--- models/itemModel.js
+++ models/itemModel.js
@@ -4,3 +4,10 @@ exports.getByCategory = (categoryId) =>
   db.query('SELECT * FROM items WHERE category_id = $1', [categoryId]);
 
 exports.getById = (id) => db.query('SELECT * FROM items WHERE id = $1', [id]);
+
+exports.create = (data) =>
+  db.query(
+    `INSERT INTO items (name, description, price, quantity, category_id)
+     VALUES ($1,$2,$3,$4,$5)`,
+    [data.name, data.description, data.price, data.quantity, data.category_id],
+  );
