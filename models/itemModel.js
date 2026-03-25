babc9709645bc1c413700108c138bd272320649d
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-03-25 21:02:45 +0100

    Add db query to get item by ID

--- models/itemModel.js
+++ models/itemModel.js
@@ -2,3 +2,5 @@ const db = require('../db');
 
 exports.getByCategory = (categoryId) =>
   db.query('SELECT * FROM items WHERE category_id = $1', [categoryId]);
+
+exports.getById = (id) => db.query('SELECT * FROM items WHERE id = $1', [id]);
