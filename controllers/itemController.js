c158a27dda481ff332e00a6993a60340c10f91c0
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-03-30 21:38:57 +0200

    Add function that gives item details

---
+++ controllers/itemController.js
@@ -0,0 +1,6 @@
+const Item = require('../models/itemModel');
+
+exports.detail = async (req, res) => {
+  const item = await Item.getById(req.params.id);
+  res.render('item_detail', { item: item.rows[0] });
+};
