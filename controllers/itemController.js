39546a9cd076568a9ea803d458125e87079b4024
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-03-31 20:50:40 +0200

    Add function that creates an item in the category

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -4,3 +4,8 @@ exports.detail = async (req, res) => {
   const item = await Item.getById(req.params.id);
   res.render('item_detail', { item: item.rows[0] });
 };
+
+exports.create = async (req, res) => {
+  await Item.create(req.body);
+  res.redirect(`/categories/${req.body.category_id}`);
+};
