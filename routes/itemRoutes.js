c4b70d21c78101a4c3b4142debe2e080bb85a32d
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-04-07 21:15:32 +0200

    Add route for deleting an item action

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -5,3 +5,4 @@ const controller = require('../controllers/itemController');
 router.get('/items/:id', controller.detail);
 router.post('/items/new', controller.create);
 router.post('/items/:id/edit', controller.update);
+router.post('/items/:id/delete', controller.delete);
