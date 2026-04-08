36da4ec8263ddeb18d3ddbcefed316a205c41e52
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-04-08 20:53:45 +0200

    Export the routes module

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -6,3 +6,5 @@ router.get('/items/:id', controller.detail);
 router.post('/items/new', controller.create);
 router.post('/items/:id/edit', controller.update);
 router.post('/items/:id/delete', controller.delete);
+
+module.exports = router;
