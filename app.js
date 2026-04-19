bbf43a9c71a0b8a96374bbcaccb8b4acbfbcf3d9
Just-AgA <161239277+Just-AgA@users.noreply.github.com>
2026-04-19 22:05:44 +0200

    Setup the view engine to use ejs files

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const categoryRoutes = require('./routes/categoryRoutes');
+const itemRoutes = require('./routes/itemRoutes');
+require('dotenv').config();
+
+const app = express();
+const expressLayouts = require('express-ejs-layouts');
+
+app.set('view engine', 'ejs');
